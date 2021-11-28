875a2c150d8023c19cc2db82f965fd377b4294e3
Ian Chadwick <ianjchadwick@gmail.com>
2021-11-28 18:21:10 -0500

    Final Updates to the FWI functions before merge

--- PythonCode/fire_weather_index.py
+++ PythonCode/fire_weather_index.py
@@ -1,5 +1,3 @@
-import xarray as xr
-import numpy as np
 import math
 """
 Modified code based on R code from: Grant Williamson:
@@ -13,7 +11,7 @@ inputs: temperature (C), relative humidity (%), average wind speed (m/s), previo
 
 Output: Today's FFMC
 """
-def fine_fuel_moisture_code(temp, rel_hum, avg_wind, precip, F_prev = 85):
+def fine_fuel_moisture_code(temp, rel_hum, avg_wind, precip, F_prev = 85.0):
 
     # Get previous day's FFMC and default to 85 if not entered
     Fo = F_prev
@@ -33,30 +31,31 @@ def fine_fuel_moisture_code(temp, rel_hum, avg_wind, precip, F_prev = 85):
             mr = 250
         mo = mr
 
-    ko = 0.424*(1 - math.pow((100-rel_hum)/100, 1.7)) + (0.0694* avg_wind^0.5)*(1 - math.pow((100-rel_hum)/100, 8))
+    ko = 0.424*(1 - math.pow((100-rel_hum)/100, 1.7)) + (0.0694* math.pow(avg_wind, 0.5))*(1 - math.pow((100-rel_hum)/100, 8))
 
     k = ko * 0.581*math.exp(0.0365*temp)
 
     Ed = 0.942*math.pow(rel_hum, 0.679) + 11*math.exp((rel_hum - 100)/10) + \
          0.18*(21.1-temp)*(1-math.exp(-0.115*rel_hum))
 
-    Ew = 0.618*math.pow(rel_hum, 0.753) + 10*math.exp((rel_hum-100)/10) + 0.18*(21.1-temp)(1-math.exp(-0.115*rel_hum))
+    Ew = 0.618*math.pow(rel_hum, 0.753) + 10*math.exp((rel_hum-100)/10) + 0.18*(21.1-temp)*(1-math.exp(-0.115*rel_hum))
 
     # If moisture is greater than Ed, than drying regime calculation takes place
     if Ed < mo:
         m = Ed + (mo - Ed)*math.pow(10, (-1*k))
 
     # If moisture is less than Ew, than wetting regime calculation takes place
-    elif mo < Ew & mo < Ed:
+    elif (mo < Ew) and (mo < Ed):
         m = Ew - (Ew - mo)*math.pow(10, (-1*k))
 
     # Else if the moisture content is between the two values, previous moisture content is today's content
-    elif Ed >= mo & Ew <= mo:
+    elif Ed >= mo and Ew <= mo:
         m = mo
 
     FFMC = 59.5* (250-m)/(147.2 + m)
 
     return FFMC
+
 
 """
 Function to calculate the Duff Moisture Code (DMC)
@@ -67,6 +66,7 @@ Output: Today's DMC
 """
 def duff_moisture_code(rel_hum, temp, precip, month, DMC_prev=6.0):
     Po = DMC_prev
+    month = int(month)
     DayLengthList = [6.5, 7.5, 9.0, 12.8, 13.9, 13.9, 12.4, 10.9, 9.4, 8.0, 7.0, 6.0]
     effDayLength = DayLengthList[month-1]
 
@@ -81,7 +81,7 @@ def duff_moisture_code(rel_hum, temp, precip, month, DMC_prev=6.0):
         # Calculate the slope variable 'b'
         if Po <= 33:
             b = 100/(0.5 + 0.3*Po)
-        elif Po > 33 & Po <= 65:
+        elif Po > 33 and Po <= 65:
             b = 14 - 1.3*math.log(Po)
         else:
             b = 6.2*math.log(Po) - 17.2
@@ -113,6 +113,7 @@ Output: Today's DC
 """
 def drought_code(temp, precip, month, DC_prev=15):
     DayLengthList = [-1.6, -1.6, -1.6, 0.9, 3.8, 5.8, 6.4, 5.0, 2.4, 0.4, -1.6, -1.6]
+    month = int(month)
     dayLengthFactor = DayLengthList[month-1]
     Do = DC_prev
 
@@ -144,4 +145,67 @@ def drought_code(temp, precip, month, DC_prev=15):
     DC = Do + 0.5*V
     return DC
 
+"""
+Function to calculate the Initial Spread Index (ISI)
+Inputs: FFMC and Wind speed
+Outputs: ISI
+"""
+def initial_spread_index(FFMC, wind):
+    # Wind function
+    fW  = math.exp(0.05039*wind)
 
+    # Get moisture from FFMC
+    m = 147.2*(101-FFMC)/(59.5 + FFMC)
+
+    # FFMC function
+
+    fF = (91.9*math.exp(-0.1386*m))*(1 + math.pow(m, 5.31)/(4.93*math.pow(10, 7)))
+
+    ISI = 0.208*fW*fF
+
+    return ISI
+
+
+"""
+Function to calculate the Buildup Index (BUI)
+Inputs: DMC and DC
+Outputs: BUI
+"""
+def buildup_index(DMC, DC):
+    # If the DMC is 0 BUI is always 0
+    if DMC == 0:
+        BUI = 0;
+        return BUI
+    if DMC <= 0.4*DC:
+        BUI = 0.8 * DMC * DC / (DMC + 0.4 * DC)
+    else:
+        BUI = DMC - (1 - 0.8*DMC/ (DMC + 0.4*DC)) * (0.92 + math.pow((0.0114*DMC), 1.7))
+
+    return BUI
+
+"""
+Final function to calculate the fire weather index FWI:
+Inputs: temperature (C), relative humidity (%), average wind speed (m/s), previous 24-hour precipitation total (mm),  
+month (as integer), previous day's DMC (default=6), previous day's FFMC (default 85), previous day's DC (default=15)
+"""
+def fire_weather_index(temp, rel_hum, wind, precip , month, DMC=6.0, FFMC=85.0, DC=15.0):
+
+    finefuelMC = fine_fuel_moisture_code(temp, rel_hum,wind, precip, FFMC)
+    duffMC = duff_moisture_code(rel_hum, temp, precip, month, DMC)
+    droughtC = drought_code(temp, precip, month, DC)
+    ISI = initial_spread_index(finefuelMC, wind)
+    BUI = buildup_index(duffMC, droughtC)
+
+    if BUI <= 80:
+        fD = 0.626 * math.pow(BUI,0.809) + 2
+    if BUI > 80:
+        fD = 1000/(25 + 108.64*math.exp(-0.023*BUI))
+
+    Bscale = 0.1*ISI*fD
+
+    if Bscale <= 1:
+        Sscale = Bscale
+    else:
+        Sscale = math.exp(2.72 * math.pow((0.434*math.log(Bscale)), 0.647))
+
+    return Sscale
